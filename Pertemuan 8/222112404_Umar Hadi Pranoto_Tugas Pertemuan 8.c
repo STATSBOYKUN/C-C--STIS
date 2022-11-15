506c33dc48316e179fd421f2750b8960fbf492b1
STATSBOYKUN <radenumarhp277@student.uns.ac.id>
2022-11-15 11:14:34 +0700

    pertemuan 9 seleseee

--- Pertemuan 8/222112404_Umar Hadi Pranoto_Tugas Pertemuan 8.c
+++ Pertemuan 8/222112404_Umar Hadi Pranoto_Tugas Pertemuan 8.c
@@ -13,9 +13,10 @@ struct node
    struct node *right;
 };
 
-int M[100][100];
+int M[500][500];
 int globalCol;
 int globalRow;
+
 int max(int num1, int num2);
 int height(struct node *root);
 int getcol(int h);
@@ -25,7 +26,7 @@ struct node *insert(struct node *root, int newData);
 struct node *delete_node(struct node *root, int deletedData);
 
 void search_node(struct node *root, int data);
-void printTree(int M[100][100], struct node *root, int col, int row, int height);
+void printTree(int M[500][500], struct node *root, int col, int row, int height);
 void TreePrinter(struct node *root);
 void displayPreorder(struct node *node);
 void displayInorder(struct node *node);
@@ -65,14 +66,27 @@ int main()
             scanf("%d", &temp);
             root = insert(root, temp);
          }
+
+         // root = insert(root, 5);
+         // root = insert(root, 1);
+         // root = insert(root, 15);
+         // root = insert(root, 9);
+         // root = insert(root, 7);
+         // root = insert(root, 12);
+         // root = insert(root, 30);
+         // root = insert(root, 25);
+         // root = insert(root, 40);
+         // root = insert(root, 40);
+         // root = insert(root, 45);
+         // root = insert(root, 42);
          break;
       case 2:
-         printf("Masukkan nama mahasiswa yang akan dihapus: ");
+         printf("Masukkan data yang akan dihapus: ");
          scanf("%d", &temp);
          root = delete_node(root, temp);
          break;
       case 3:
-         printf("Masukkan nama mahasiswa yang akan dicari: ");
+         printf("Masukkan data yang akan dicari: ");
          scanf("%d", &temp);
          search_node(root, temp);
          break;
@@ -165,27 +179,36 @@ void search_node(struct node *root, int data)
 {
    struct node *cursor = root;
 
-   while (cursor->data != data)
+   if (cursor == NULL)
    {
-      if (cursor != NULL)
+      printf("Node %d tidak ditemukan", data);
+      return;
+   }
+   else
+   {
+      while (cursor->data != data)
       {
-         if (data > cursor->data)
+         if (cursor != NULL)
          {
-            cursor = cursor->right;
-         }
-         else
-         {
-            cursor = cursor->left;
-         }
+            if (data > cursor->data)
+            {
+               cursor = cursor->right;
+            }
+            else
+            {
+               cursor = cursor->left;
+            }
 
-         if (cursor == NULL)
-         {
-            printf("\nNode %d tidak ditemukan", data);
-            return;
+            if (cursor == NULL)
+            {
+               printf("\nNode %d tidak ditemukan", data);
+               return;
+            }
          }
       }
    }
 
+   root = cursor;
    printf("\nNode %d ditemukan", data);
    return;
 }
@@ -227,15 +250,16 @@ struct node *delete_node(struct node *root, int deletedData)
       }
    }
 
-   return cursor;
+   return root;
 }
 
 int max(int num1, int num2)
 {
-   return (num1 > num2 ) ? num1 : num2;
+   return (num1 > num2) ? num1 : num2;
 }
 
-int height(struct node *root){
+int height(struct node *root)
+{
    if (root == NULL)
    {
       return 0;
@@ -246,83 +270,61 @@ int height(struct node *root){
    }
 }
 
-int getcol(int h){
+int getcol(int h)
+{
    if (h == 1)
       return 1;
-   return getcol(h-1) + getcol(h-1) + 1;
+   return getcol(h - 1) + getcol(h - 1) + 1;
 }
 
-void printTree(int M[100][100], struct node *root, int col, int row, int height){
+void printTree(int M[500][500], struct node *root, int col, int row, int height)
+{
    if (root == NULL)
    {
       return;
    }
-   
-   M[row*2][col+1] = root->data;
-   globalCol = col+1;
-   globalRow = row*2;
 
-   printTree(M, root->left, col-pow(2, height-2), row+1, height-1);
-   printTree(M, root->right, col+pow(2, height-2), row+1, height-1);
+   M[row][col] = root->data;
+   globalCol = col;
+   globalRow = row;
+
+   printTree(M, root->left, col - pow(2, height - 2), row + 1, height - 1);
+   printTree(M, root->right, col + pow(2, height - 2), row + 1, height - 1);
 }
 
-void TreePrinter(struct node *root){
+void TreePrinter(struct node *root)
+{
    int i = 0, j = 0;
    int h = height(root);
    int col = getcol(h);
 
-   printTree(M, root, floor(col/2), 0, h);
+   printTree(M, root, floor(col / 2), 0, h);
 
-   for (i = 0; i <= globalRow; i++)
+   for (i = 0; i <= globalRow + 1; i++)
    {
-      for (j = 0; j <= globalCol; j++)
+      for (j = 0; j <= globalCol + 1; j++)
       {
          M[i][j] = 0;
       }
    }
-   printTree(M, root, floor(col/2), 0, h);
 
-   // int rootColumn = 0;
-   // for (i = 0; i <= globalRow; i++)
-   // {
-   //    for (j = 0; j <= globalCol; j++)
-   //    {
-   //       roo
-   //       if (i > 1){
-   //          if ((M[i][j] != 0) && (possibleTwoNodes == 0)){
-   //             // M[i-1][j+2] = 2;
-   //             M[i-1][j+2] = 100001;
-   //             // M[i+1][j+2] = 2;
-   //             possibleTwoNodes = 1;
-   //          } else if ((M[i][j] != 0) && (possibleTwoNodes == 1)){
-   //             // M[i-1][j-2] = 3;
-   //             M[i-1][j+2] = 111110;
-   //             // M[i+1][j-2] = 3;
-   //          }
-   //       }
-   //    }
-   //    possibleTwoNodes = 0;
-   // }
-   
-   for (i = 0; i <= globalRow; i++)
+   printTree(M, root, floor(col / 2), 0, h);
+
+   for (i = 0; i <= globalRow + 1; i++)
    {
-      for (j = 0; j <= globalCol; j++){
-         if (M[i][j] == 0){
+      for (j = 0; j <= globalCol + 1; j++)
+      {
+         if (M[i][j] == 0)
+         {
             printf(" ");
-         }
-         else if (M[i][j] == 100001){
-            printf("/");
-         }
-         else if (M[i][j] == 111110){
-            printf("\\");
          }
          else
          {
-           printf("%d ", M[i][j]);
+            printf("%d ", M[i][j]);
          }
       }
-      
-      printf("\n");
+
+      printf("\n\n");
    }
 }
 
--- Pertemuan 9/tugas.c
+++ Pertemuan 9/tugas.c
@@ -5,7 +5,7 @@
 #include <math.h>
 
 #define CLEAR system("cls")
-struct node
+struct node 
 {
    int data;
    int height;
@@ -13,49 +13,11 @@ struct node
    struct node *right;
 };
 
-int max(int a, int b);
-int getHeight(struct node *N);
-int getBalanceFactor(struct node *N);
-int height(struct node *node);
-
-struct node *newNode(int data);
-struct node *rightRotate(struct node *T);
-struct node *leftRotate(struct node *T);
-struct node *insert(struct node *root, int new_data);
-struct node *delete_node(struct node *root, int data);
-
-void printGivenLevel(struct node *root, int level);
-void printLevelOrder(struct node *root);
-void displayPreorder(struct node *node);
-
-int main()
-{
-   struct node *root = NULL;
-   root = insert(root, 9);
-   root = insert(root, 5);
-   root = insert(root, 10);
-
-   root = insert(root, 0);
-   root = insert(root, 6);
-   root = insert(root, 11);
-   root = insert(root, -1);
-   root = insert(root, 1);
-   root = insert(root, 2);
-
-   root = delete_node(root, 10);
-   displayPreorder(root);
-   printf("\n");
-   printLevelOrder(root);
-
-   return 0;
-}
-
-struct node *newNode(int data)
-{
+struct node *newNode(int data) {
    struct node *node = (struct node *)malloc(sizeof(struct node));
 
    node->data = data;
-   node->height = 1;
+   node->height = 1; 
    node->left = NULL;
    node->right = NULL;
    return node;
@@ -94,7 +56,7 @@ struct node *rightRotate(struct node *T)
    T->left = orphan;
 
    // Update height
-   T->height = max(getHeight(T->left), getHeight(T->right)) + 1;
+   T->height = max(getHeight(T->left), getHeight(T -> right)) + 1;
    new_root->height = max(getHeight(new_root->left), getHeight(new_root->right)) + 1;
 
    // Return root baru
@@ -111,9 +73,9 @@ struct node *leftRotate(struct node *T)
    T->right = orphan;
 
    // Update height
-   T->height = max(getHeight(T->left), getHeight(T->right)) + 1;
+   T->height = max(getHeight(T->left), getHeight(T -> right)) + 1;
 
-   new_root->height = max(getHeight(new_root->left), getHeight(new_root->right)) + 1;
+   new_root->height = max(getHeight(new_root->left),getHeight(new_root->right)) + 1;
 
    // Return root baru
    return new_root;
@@ -133,7 +95,7 @@ struct node *insert(struct node *root, int new_data)
 
    // 2. Update height dari node baru sampai root
    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
-   // 3. Hitung balance factor untuk menentukan apakahnode unbalanced
+   // 3. Hitung balance factor untuk menentukan apakahnode unbalanced 
    int balance = getBalanceFactor(root);
 
    // Jika tidak balanced, return hasil rotation
@@ -201,13 +163,12 @@ struct node *delete_node(struct node *root, int data)
       }
    }
 
-   // Jika setelah dilakukan delete, tree kosong maka return root
-   if (root == NULL)
-      return root;
+   // Jika setelah dilakukan delete, tree kosong maka return root 
+   if (root == NULL) return root;
    // 2. Update height dari node
    root->height = 1 + max(getHeight(root->left), getHeight(root->right));
 
-   // 3. Hitung balance factor untuk menentukan apakah root unbalanced
+   // 3. Hitung balance factor untuk menentukan apakah root unbalanced 
    int balance = getBalanceFactor(root);
 
    // Jika tidak balanced, return hasil rotation
@@ -236,61 +197,31 @@ struct node *delete_node(struct node *root, int data)
    return root;
 }
 
-void printGivenLevel(struct node *root, int level)
-{
-   if (root == NULL)
-      return;
-   if (level == 1)
-      printf("%d ", root->data);
-   else if (level > 1)
-   {
-      printGivenLevel(root->left, level - 1);
-      printGivenLevel(root->right, level - 1);
-   }
-}
-
-void printLevelOrder(struct node *root)
-{
-   int h = height(root);
-   int i;
-   for (i = 1; i <= h; i++)
-   {
-      printGivenLevel(root, i);
-      printf("\n");
-   }
-}
-
-/* Compute the "height" of a tree -- the number of
- nodes along the longest path from the root node
- down to the farthest leaf node.*/
-int height(struct node *node)
-{
-   if (node == NULL)
-      return 0;
-   else
-   {
-      /* compute the height of each subtree */
-      int lheight = height(node->left);
-      int rheight = height(node->right);
-
-      /* use the larger one */
-      if (lheight > rheight)
-      {
-         return (lheight + 1);
-      }
-      else
-      {
-         return (rheight + 1);
-      }
-   }
-}
-
-void displayPreorder(struct node *node)
-{
+void displayPreorder(struct node *node) {
    if (node == NULL)
       return;
 
    printf("%d ", node->data);    // root
    displayPreorder(node->left);  // subtree kiri
    displayPreorder(node->right); // subtree kanan
+}
+
+int main()
+{
+   struct node *root = NULL;
+   root = insert(root, 9);
+   root = insert(root, 5);
+   root = insert(root, 10);
+
+   root = insert(root, 0);
+   root = insert(root, 6);
+   root = insert(root, 11);
+   root = insert(root, -1);
+   root = insert(root, 1);
+   root = insert(root, 2);
+
+   root = delete_node(root, 10);
+   displayPreorder(root);
+
+   return 0;
 }
